7d81960cd1378627c994fb7cfe0b43213180265d
Jan Pazdziora <jpazdziora@redhat.com>
2018-07-17 00:21:52 +0200

    Wait for the HTTP server to start in tests.

--- tests/run.sh
+++ tests/run.sh
@@ -3,6 +3,14 @@
 set -e
 set -x
 
+echo "Wait for the HTTP server to start ..."
+for i in $( seq 1 10 ) ; do
+	if curl -s -o /dev/null http://localhost/ ; then
+		break
+	fi
+	sleep 3
+done
+
 echo "Testing Require pam-account"
 curl -s -D /dev/stdout -o /dev/null http://localhost/authz | tee /dev/stderr | grep 401
 curl -u alice:Tajnost -s -D /dev/stdout -o /dev/null http://localhost/authz | tee /dev/stderr | grep 401
